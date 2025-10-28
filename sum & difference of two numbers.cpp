6cce6b3f7e5d8170e4452ce9c308ebb4371b7492
anuhya-2008 <anuhyakeerthikajjayam@gmail.com>
2025-10-28 22:23:26 +0530

    Add files via upload

---
+++ sum & difference of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,sum,diff;
+	printf("enter a & b values:");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	diff=a-b;
+    printf("\nsum:%d",sum);
+    printf("\ndifference:%d",diff);
+    return 0;
+}
+
