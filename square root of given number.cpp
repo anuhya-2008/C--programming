95383662a0d733a9b9489b83ee1f8b93d673dd96
anuhya-2008 <anuhyakeerthikajjayam@gmail.com>
2025-10-28 23:29:06 +0530

    Add files via upload

---
+++ square root of given number.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double a,root;
+	printf("enter number:");
+	scanf("%lf",&a);
+	root=sqrt(a);
+	printf("square root of %.lf:%.lf",a,root);
+	return 0;
+}
