752163deb8976c6aa67ed18f55be3e871fb7be34
anuhya-2008 <anuhyakeerthikajjayam@gmail.com>
2025-11-10 18:50:49 +0530

    Add files via upload

---
+++ Upper traingular matrix.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	int a[100][100],i,j,n,sum=0;
+	printf("enter array size:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("\nenter value at position a[%d][%d]:",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("\nThe upper traingular matrix is\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i>j)
+			{
+				printf("\t0 ");
+			}
+			else
+			{
+				printf("\t%d",a[i][j]);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
