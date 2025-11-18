93b4345b256f863373ddbb3f537b35b3b4b0e82b
anuhya-2008 <anuhyakeerthikajjayam@gmail.com>
2025-11-18 21:58:13 +0530

    Add files via upload

---
+++ copy the srting.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	char a[100],b[100];
+	int i;
+	printf("Enter any string:");
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		b[i]=a[i];
+	}
+	printf("\nCopied data:%s",b);
+	return 0;
+}
