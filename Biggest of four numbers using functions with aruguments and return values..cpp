58975d0b48816628196062b1f6084f1f2a4d654a
anuhya-2008 <anuhyakeerthikajjayam@gmail.com>
2025-11-21 11:00:57 +0530

    Add files via upload

---
+++ Biggest of four numbers using functions with aruguments and return values..cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int biggest(int a,int b,int c,int d);
+int main() 
+{
+    int a,b,c,d,result;
+    printf("\nenter a,b,c &d values:");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    result=biggest(a,b,c,d);
+    printf("%d is the biggest number", result);
+    return 0;
+}
+int biggest(int a,int b,int c,int d)
+{
+    if(a>b && a>c && a>d)
+    return a;
+    else if(b>c && b>d)
+    return b;
+    else if(c>d)
+    return c;
+    else
+    return d;
+}
